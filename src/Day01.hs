521e7533db97ea51da447969b6395736c16d62ef
Matt Perry <muffinman616@gmail.com>
2018-12-01 00:26:26 -0500

    Finish Day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,6 +1,25 @@
 module Day01 where
 
+import Data.Set (Set)
+import qualified Data.Set as S
+
+parse :: String -> Int
+parse = read . filter (/= '+')
+
+dupe :: Set Int -> [Int] -> Int
+dupe seen (x:xs)
+    | x `S.member` seen = x
+    | otherwise         = dupe (S.insert x seen) xs
+dupe _ _ = error "duplicate not found"
+
+part1 :: [Int] -> Int
+part1 = sum
+
+part2 :: [Int] -> Int
+part2 = dupe S.empty . scanl (+) 0 . cycle
+
 main :: IO ()
 main = do
-    input <- readFile "input/Day01.txt"
-    print input
+    input <- map parse . lines <$> readFile "input/Day01.txt"
+    print . part1 $ input
+    print . part2 $ input
